f59f9c0ef2c3320e752da6e6e2718a992136c84a
Raphael Mercier <raphaelmercier@softguard007.com>
2025-09-20 13:18:08 -0400

    Adapting pseudocode for our implementation

--- A1/assignment1.py
+++ A1/assignment1.py
@@ -1,6 +1,6 @@
 # Name this file to assignment1.py when you submit
 import pandas as pd
-
+MOVING_COST = 1
 # The pathfinding function must implement A* search to find the goal state
 def pathfinding(filepath):
   csv = pd.read_csv(filepath, header=None)  # header=None since there are no column headers
@@ -22,43 +22,41 @@ def pathfinding(filepath):
       elif str(cell_value).isdigit() and cell_value != "0":
         treasures.append((x, y, int(cell_value)))
 
+  if (start == None or len(goals) <= 0):
+    return False
 
-  # TODO: Implement A* search algorithm here
-  # For now, return placeholder values
-  optimal_path = []  # Will contain the path from start to goal
-  optimal_path_cost = 0  # Will contain the total cost of the path
-  num_states_explored = 0  # Will contain number of states explored during search
+  explored = []
+  frontier = [start]
+  optimal_path = []
+  optimal_path_cost = 0
+
+  while True:
+    leaf = frontier.pop()
+
+    if leaf in goals:
+      break
+
+    explored.append (leaf)
+    for node in neighbourhood(graph, leaf):
+      curr_path_cost = leaf.path_cost + MOVING_COST + leaf.heuristic
+      if (node not in frontier and node not in explored or node in frontier and curr_path_cost < node.path_cost + node.heuristic):
+        node.parent = leaf
+        node.path_cost = leaf.path_cost + MOVING_COST 
+        frontier.append(node)
+
+  # TODO: We will probably need to have a class: node, 
+  # members should be: path cost, heuristic, parent, type(start, goal, wall, treasure(should also have treasure value here))
 
   # optimal_path is a list of coordinate of squares visited (in order)
   # optimal_path_cost is the cost of the optimal path
   # num_states_explored is the number of states explored during A* search
-  return optimal_path, optimal_path_cost, num_states_explored
+  return optimal_path, optimal_path_cost, len(explored)
 
 
-
-
+# TODO: implement the neighbourhood function, 
+# it should determine which nodes you can travel to
+def neighbourhood(graph, leaf):
+  return []
 
 
 pathfinding("/Users/raphaelmercier/Documents/COMP 3106/COMP-3106/A1/Examples/Example0/grid.txt")
-
-
-# uniform cost search pseudo
-def graph_search(graph, start_states, goal_states):
-  frontier = start_states
-  explored = []
-  while True:
-    if frontier == []:
-      return False
-
-    leaf = frontier.pop()
-
-    if leaf in goal_states:
-      return path
-
-    explored.append (leaf)
-    for node in neighbourhood (graph, leaf):
-      curr_path_cost = leaf.path_cost + edge_weight(leaf, node) 
-      if (node not in frontier and node not in explored or node in frontier and curr_path_cost < node.path_cost):
-        node.parent = leaf
-        node.path_cost = curr_path_cost 
-        frontier.append (node)
