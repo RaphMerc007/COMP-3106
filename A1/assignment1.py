1614cc7d4e073e8fa41b8defa19ce10581afefc3
Raphael Mercier <raphaelmercier@softguard007.com>
2025-09-21 18:55:33 -0400

    Adding node class and implementing neighbourhood function

--- A1/assignment1.py
+++ A1/assignment1.py
@@ -15,13 +15,13 @@ def pathfinding(filepath):
     for y in range(len(graph[x])):
       cell_value = graph[x][y] 
       if cell_value == "S": 
-        start = (x, y)
+        start = Node((x, y), "S", heuristic=heuristic((x, y), goals[0].position))
       elif cell_value == "G":
-        goals.append((x, y))
+        goals.append(Node((x, y), "G"))
       elif cell_value == "X":
-        walls.append((x, y))
+        walls.append(Node((x, y), "X"))
       elif str(cell_value).isdigit() and cell_value != "0":
-        treasures.append((x, y, int(cell_value)))
+        treasures.append(Node((x, y), cell_value))
 
   # if there is no start or no goals path cannot be found
   if (start == None or len(goals) <= 0):
@@ -37,18 +37,18 @@ def pathfinding(filepath):
   # TODO: we need to somehow consider how to gather at least 5 treasure points before finishing
   while True:
     leaf = frontier.pop()
+
     if leaf in goals:
       break
-    explored.append (leaf)
-    for node in neighbourhood(graph, leaf):
+    explored.append(leaf)
+    for node in neighbourhood(graph, explored, leaf):
       curr_path_cost = leaf.path_cost + MOVING_COST + leaf.heuristic
       if (node not in frontier and node not in explored or node in frontier and curr_path_cost < node.path_cost + node.heuristic):
         node.parent = leaf
         node.path_cost = leaf.path_cost + MOVING_COST 
+        node.heuristic = heuristic(node.position, goals[0].position)
         frontier.append(node)
 
-  # TODO: We will probably need to have a class: node, 
-  # members should be: path cost, heuristic, parent, type(start, goal, wall, treasure(should also have treasure value here))
 
   # optimal_path is a list of coordinate of squares visited (in order)
   # optimal_path_cost is the cost of the optimal path
@@ -56,12 +56,61 @@ def pathfinding(filepath):
   return optimal_path, optimal_path_cost, len(explored)
 
 
-# TODO: implement the neighbourhood function, 
-# it should determine which nodes you can travel to
-# Should also determine heuristic of nodes, 
-# i did some research and recommend Manhattan distance: |leaf.x - goal.x| + |leaf.y - goal.y|
-def neighbourhood(graph, leaf):
-  return []
+def neighbourhood(graph, explored, leaf):
+  x = leaf.position[0]
+  y = leaf.position[1]
+  neighbours = []
+  if (x > 0):
+    if (graph[x-1][y] != "X" and Node((x-1, y)) not in explored):
+      neighbours.append(Node((x-1, y), graph[x-1][y]))
+  if (y > 0):
+    if (graph[x][y-1] != "X" and Node((x, y-1)) not in explored):
+      neighbours.append(Node((x, y-1), graph[x][y-1]))
+  if (x < len(graph)-1):
+    if (graph[x+1][y] != "X" and Node((x+1, y)) not in explored):
+      neighbours.append(Node((x+1, y), graph[x+1][y]))
+  if (y < len(graph[x])-1):
+    if (graph[x][y+1] != "X" and Node((x, y+1)) not in explored):
+      neighbours.append(Node((x, y+1), graph[x][y+1]))
+  return neighbours
+
+
+def heuristic(position, goal):
+  # Manhattan distance
+  return abs(position[0] - goal[0]) + abs(position[1] - goal[1])
+
+
+
+class Node :
+  def __init__(self, position, type="0", path_cost=0, heuristic=0 ,parent=None, value=0, explored=False) -> None:
+    self.position = position
+    self.path_cost = path_cost
+    self.heuristic = heuristic
+    self.parent = parent
+    self.explored = explored
+
+    # special case for treasures
+    if (self.type.isdigit() and self.type != "0"):
+      self.value = int(self.type)
+      self.type = "T"
+    else:
+      self.type = type
+      self.value = value
+
+  def __eq__(self, other):
+    return self.position == other.position
+    
+  def __lt__(self, other):
+    # Define less-than for priority queue sorting
+    return self.heuristic + self.path_cost < other.heuristic + other.path_cost
+
+
+
+
+
+
+
+
 
 
 pathfinding("/Users/raphaelmercier/Documents/COMP 3106/COMP-3106/A1/Examples/Example0/grid.txt")
