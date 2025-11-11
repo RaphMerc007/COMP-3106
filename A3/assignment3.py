c133e90e65a3aeb24ec3b2e3320fcbc51c7dfd0e
Raphael Mercier <raphaelmercier@softguard007.com>
2025-11-11 12:25:15 -0500

    Randomize choice when no has not been seen yet

--- A3/assignment3.py
+++ A3/assignment3.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 
 class td_qlearning:
@@ -88,7 +89,8 @@ class td_qlearning:
     best_action = None
 
     if state not in self.qfunction:
-      return 
+      # can pick any action
+      return random.randint(1, 3)
 
     for action in self.qfunction[state]:
       qvalue = self.qvalue(state, action)
